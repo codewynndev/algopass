3f2cf6bea9b584be9336848843a36f25aa240222
rSaydelleS <rafael.ppuerari@gmail.com>
2024-12-07 20:36:32 -0300

    feat: aplicando o bcrypt

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,6 @@
+import passwordEncypter from "../controllers/encrypt.js";
 const routes = (fastify, options, done) => {
-  fastify.get("/", (req, reply) => {
-    return reply.status(200).send({ data: "Rota raiz" });
-  });
+  fastify.get("/", passwordEncypter);
 
   fastify.post("/create", (req, reply) => {
     return reply.status(201).send({ data: "Rota de gerar criptografia" });
