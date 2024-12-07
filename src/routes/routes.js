908462884cfb60692cd04ebb73c28a2ce0cffd10
dudamarqs <eduardamarquesnoleto@gmail.com>
2024-12-07 20:28:42 -0300

    feat: adicionar rotas GET e PUT

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,19 @@
 const routes = (fastify, options, done) => {
+
+  // Rota GET (obter informações):
   fastify.get("/", (req, reply) => {
     return reply.status(200).send({ data: "Rota raiz" });
   });
 
-  fastify.post("/create", (req, reply) => {
-    return reply.status(201).send({ data: "Rota de gerar criptografia" });
-  });
+  // Rota PUT (atualizar recurso):
+  fastify.put("/update/:id", (req, reply) => { 
+    const { id } = req.params;
+    const { content } = req.body;
 
-  fastify.delete("/delete", (req, reply) => {
-    return reply.status(200).send({ data: "Rota de deletar" });
+    if (!content) {
+      return reply.status(400).send({error: "Conteúdo não fornecido."});
+    }
+    return reply.status(200).send({data: `Recurso com ID ${id} atualizado com o conteúdo: ${content}.`});
   });
 
   done();
