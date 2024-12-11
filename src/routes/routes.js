56922edca747685f2e5c33eccbab53e4bdedae4e
EricSantoos <contatoericsantos212@gmail.com>
2024-12-10 21:04:10 -0300

    feat:criacao-post

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -16,7 +16,23 @@ const routes = (fastify, options, done) => {
     return reply.status(200).send({data: `Recurso com ID ${id} atualizado com o conteúdo: ${content}.`});
   });
 
+  //  rota de crição post 
+  fastify.post("/senha", (req, reply) => {
+    const {senha} = req.body;
+  
+    if (!senha) {
+      return reply.status(400).send({ message: "Senha é obrigatória." });
+    }
+    
+    // senhas.push(senha);
+  
+    reply.send({ message: "Senha adicionada com sucesso!", senha });
+  
+  })
+  
   done();
+
 };
+
 
 export default routes;
