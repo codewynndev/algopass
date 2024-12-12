aa9b6410571f90b7e89b2647989fffa01fc79d06
vinicius <viniamenic@gmail.com>
2024-12-11 22:47:42 -0300

    Testes de crpitografia e requisições das rotas

---
+++ src/controllers/cripto.js
@@ -0,0 +1,28 @@
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+
+dotenv.config(); // Carrega as variáveis do arquivo .env
+
+const numeroDeSalts = parseInt(process.env.NUMERO_DE_SALTS) || 10;
+
+
+ // Função para criptografar uma senha.
+ // @param {string} senha - A senha a ser criptografada.
+ // @returns {Promise<string>} - Retorna a senha criptografada.
+ 
+const criptografarSenha = async (senha) => 
+    {
+
+  try 
+  {
+    const senhaCriptografada = await bcrypt.hash(senha, numeroDeSalts);
+    return senhaCriptografada;
+  } 
+  catch (erro) 
+  {
+    console.error("Erro ao criptografar a senha:", erro);
+    throw new Error("Erro ao processar a senha.");
+  }
+};
+
+export default criptografarSenha;
